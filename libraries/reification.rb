6776bedb3b638ab891e06cd6bc184b43a7132e3f
Sean Clemmer <sczizzo@gmail.com>
2015-01-21 09:26:14 -0800

    Added "reify_each" to simplify implementation of "reify_packages"

--- libraries/reification.rb
+++ libraries/reification.rb
@@ -25,33 +25,14 @@ module Reification
     end
   end
 
+  def reify_each resource, specs, notifications=[], actions=[]
+    specs.each do |name, spec|
+      reify resource, spec.merge({ name: name }), notifications, actions
+    end
+  end
 
   def reify_packages packages, notifications=[], actions=[]
-    packages.to_hash.each do |name, spec|
-      next unless spec
-      package name do
-
-        spec.each do |k, v|
-          send k.to_sym, v
-        end
-
-        notifications.each do |notify_spec|
-          raise 'Not a valid notification spec' unless notify_spec.is_a?(Array)
-          notify_spec     = notify_spec.dup
-          notify_action   = notify_spec.shift.to_sym
-          notify_resource = notify_spec.shift
-          notify_timing   = notify_spec.shift || :delayed
-          send :notifies, \
-            notify_action, notify_resource, notify_timing
-        end
-
-        actions = [ actions ] unless actions.is_a?(Array)
-        actions.each do |action|
-          send :action, action.to_sym
-        end
-
-      end
-    end
+    reify_each :package, packages, notifications, actions
   end
 end
 

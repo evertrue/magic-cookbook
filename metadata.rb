d13fbcf4476aeabe323fc28c0cb54e1e5203d9db
Eddie Hurtig <EdHurtig@users.noreply.github.com>
2015-09-19 00:15:37 -0400

    Rename cookbook to et_magic cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,4 @@
-name             'magic'
+name             'et_magic'
 version          File.read(File.join(File.dirname(__FILE__), 'VERSION')).strip
 description      'Cookbook helpers and other magical things'
 long_description 'Cookbook helpers and other magical things'

eb509f219ebbea767affbb339110de6f7a619863
Eddie Hurtig <EdHurtig@users.noreply.github.com>
2015-09-19 00:15:12 -0400

    Rip out this logstash_config function that is making me mad

--- libraries/configuration.rb
+++ libraries/configuration.rb
@@ -136,56 +136,6 @@ module Configuration
       obj.inspect.gsub('\\\\', '\\')
     end
   end
-
-  def logstash_config obj
-    tpl = %Q$
-      input {
-      <% obj['input'].each do |type, inputs| %>
-      <% inputs.each do |name, config| %>
-        <%= name %> {<% config.each do |k, v| %>
-          <%= k %> => <%= quote_logstash v %><% end %>
-          <% if type.is_a?(String) %>type => <%= quote_logstash type %><% end %>
-        }
-      <% end %><% end %>
-      }
-
-      filter {
-      <% obj['filter'].each do |type, filters| %>
-        <% type = type.strip if type.is_a?(String) %>
-        <% if type.is_a?(String) && type =~ /^(if|else)/ %><%= type %> {
-        <% elsif type.is_a?(String) %>if [type] == <%= quote_logstash type %> {<% end %>
-        <% filters.each do |name, config| %>
-          <% if name.is_a?(String) && name =~ /^(if|else)/ %>
-          <%= name %> {<%= "\n" if config.keys.empty? %><% config.each do |k, v| %>
-            <%= k %> {<%= "\n" if config[k].keys.empty? %><% config[k].each do |k2, v2| %>
-              <%= k2 %> => <%= quote_logstash v2 %><% end %>
-            }<% end %>
-          }
-          <% elsif name.is_a?(String) %>
-          <%= name %> {<%= "\n" if config.keys.empty? %><% config.each do |k, v| %>
-            <%= k %> => <%= quote_logstash v %><% end %>
-          }
-          <% else %>
-            <%= quote_logstash config %>
-          <% end %>
-      <% end %><% if type.is_a?(String) %>  }
-      <% end %><% end %>
-      }
-
-      output {
-      <% obj['output'].each do |type, outputs| %>
-        <% if type.is_a?(String) && type =~ /^(if|else)/ %><%= type %> {
-        <% elsif type.is_a?(String) %>if [type] == <%= quote_logstash type %> {<% end %>
-        <% outputs.each do |name, config| %>
-          <%= name %> {<% config.each do |k, v| %>
-            <%= k %> => <%= quote_logstash v %><% end %>
-          }
-      <% end %><% if type.is_a?(String) %>  }
-      <% end %><% end %>
-      }
-    $.strip.gsub(/^      /, '')
-    ERB.new(tpl).result(binding).gsub(/(\n\s*)+?\n/, "\n")
-  end
 end
 
 class Chef
